a7e2f0c26e3b083a9d5a72df19df30a9ad68a3f0
USudhakarRane <147273543+USudhakarRane@users.noreply.github.com>
2023-10-08 21:22:16 +0530

    Add files via upload

---
+++ Lab006UserInput.py
@@ -0,0 +1,19 @@
+#How to take a user input:
+
+#Task1:
+name = input("Enter your name")
+print(name)
+
+#Task2:
+fn = input("Enter First Name")
+ln = input("Enter Last Name")
+print(fn,ln,sep="-",end="\t")
+
+#Task3:
+name = input("Enter your name")
+print("You are welcome.",name)
+
+
+
+#Notes:
+#Type the value in console, and the entered value will gets printed
